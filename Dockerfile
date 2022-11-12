531eee5fcbd5f9afa0f73a38f542a51867ad35e4
Алексей <click742@yandex.ru>
2022-11-12 19:23:41 +0300

    Add nginx.conf and run all containers on localhost

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,5 @@ COPY requirements.txt .
 RUN python -m pip install --upgrade pip && pip install -r /syzhet_api/requirements.txt --no-cache-dir
 
 COPY . .
+
+CMD ["uvicorn", "backend.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from .config import base_config
@@ -10,6 +11,14 @@ app.include_router(
     router=router,
     prefix='/api/v1'
 )
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 if __name__ == '__main__':
     uvicorn.run(
