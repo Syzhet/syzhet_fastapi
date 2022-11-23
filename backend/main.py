e385136ff17f33cbc7d12ed21c0c943c99144012
Алексей <click742@yandex.ru>
2022-11-23 11:49:50 +0300

    Add docstring in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,15 @@
-# from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from fastapi import FastAPI
 
 from .api.routers import router
 from .config import base_config
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 
 app.include_router(
     router=router,
     prefix='/api/v1'
 )
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 if __name__ == '__main__':
     uvicorn.run(
