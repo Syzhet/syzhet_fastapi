f24506e5c82fb34e9a26f885eebc6a8711b0685a
Алексей <click742@yandex.ru>
2022-11-23 11:11:14 +0300

    Add docstring in base.py

--- backend/db/base.py
+++ backend/db/base.py
@@ -1,35 +1,43 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
+                                    create_async_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from backend.config import base_config
 
-PG_USER = base_config.db.db_user
-PG_PASS = base_config.db.db_password
-PG_HOST = base_config.db.db_host
-PG_PORT = base_config.db.db_port
-DATABASE = base_config.db.database
+PG_USER: str = base_config.db.db_user
+PG_PASS: str = base_config.db.db_password
+PG_HOST: str = base_config.db.db_host
+PG_PORT: str = base_config.db.db_port
+DATABASE: str = base_config.db.database
 
 
-DATABASE_URL = (
+DATABASE_URL: str = (
     f"postgresql+asyncpg://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{DATABASE}"
 )
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
 
-async_session = sessionmaker(
+async_session: AsyncSession = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
 
 Base = declarative_base()
 
 
-async def init_models():
+async def init_models() -> None:
+    """
+    Function for deleting old tables from the database
+    and creating new ones.
+    """
+
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
-async def get_session():
+async def get_session() -> AsyncSession:
+    """Function for creating a database connection session."""
+
     async with async_session() as session:
         yield session
