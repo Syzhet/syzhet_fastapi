38708bd21afd9bf7fd23350f98dd415875e33939
Алексей <click742@yandex.ru>
2022-11-23 10:53:52 +0300

    Add docstring in auth.py

--- backend/auth/auth.py
+++ backend/auth/auth.py
@@ -8,24 +8,30 @@ from ..db.models.admin import Admin
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def verify_password(plain_password, hashed_password):
-    """Сравнивает переданный пароль и хеш-пароль хранящийся в базе."""
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Compares the transmitted password and
+    the hash password stored in the database.
+    """
 
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_password_hash(password):
-    """Преобразует пароль пользователя в хеш-пароль для хранения в базе."""
+def get_password_hash(password: str) -> str:
+    """
+    Converts the user's password into a hash password
+    for storage in the database.
+    """
 
     return pwd_context.hash(password)
 
 
-async def get_admin(session: AsyncSession, username: str):
-    """Функция возвращает объект юзера из базы."""
+async def get_admin(session: AsyncSession, username: str) -> Admin:
+    """Returns the administrator object from the database."""
 
     query = select(Admin).where(Admin.username == username)
     obj = await session.execute(query)
-    admin = obj.scalars().first()
+    admin: Admin = obj.scalars().first()
     if not admin:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -38,10 +44,10 @@ async def authenticate_admin(
     session: AsyncSession,
     username: str,
     password: str
-):
-    """Аутентификация проверка юзернейма и пароля."""
+) -> Admin:
+    """Authentication, verification of the username and password."""
 
-    admin = await get_admin(session, username)
+    admin: Admin = await get_admin(session, username)
     if not verify_password(password, admin.hashed_password):
         return False
     return admin
